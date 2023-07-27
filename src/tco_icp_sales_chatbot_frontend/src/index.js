0c47a93306f69e7617822b40d04be758ef35a054
Mario Garcia <mariogarcia.ar@gmail.com>
2023-07-27 16:34:52 -0300

    Default motoko installation

---
+++ src/tco_icp_sales_chatbot_frontend/src/index.js
@@ -0,0 +1,19 @@
+import { tco_icp_sales_chatbot_backend } from "../../declarations/tco_icp_sales_chatbot_backend";
+
+document.querySelector("form").addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const button = e.target.querySelector("button");
+
+  const name = document.getElementById("name").value.toString();
+
+  button.setAttribute("disabled", true);
+
+  // Interact with foo actor, calling the greet method
+  const greeting = await tco_icp_sales_chatbot_backend.greet(name);
+
+  button.removeAttribute("disabled");
+
+  document.getElementById("greeting").innerText = greeting;
+
+  return false;
+});
